a4785c73e2c732e65c67610808e8ddb35b68f9cd
Tim Long <tim.long@jhuapl.edu>
2023-03-28 15:32:02 -0400

    remove unused files

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,9 @@ import ScrollyVideo from 'scrolly-video/dist/ScrollyVideo.vue'
 
 <template>
     <main class="h-[2000px]">
-        <div class="fixed h-screen w-screen z-10 p-32">
+        <div class="fixed z-10 w-screen h-screen p-32">
             <h1 class="text-white text-9xl drop-shadow-sm">🥳</h1>
-            <p class="text-white/75 text-xs font-bold uppercase tracking-widest absolute bottom-0 mb-32">[Scroll Up and Down]</p>
+            <p class="absolute bottom-0 mb-32 text-xs font-bold tracking-widest uppercase text-white/75">[Scroll Up and Down]</p>
         </div>
         <ScrollyVideo :src="BgVideo" :debug="true" />
     </main>
