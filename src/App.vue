e14e73937437c8c3c102f954ea81811f0231dd6e
Tim Long <tim.long@jhuapl.edu>
2023-03-28 15:26:43 -0400

    init commit - working example w/ vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import BgVideo from './assets/bg-video.mp4'
+import ScrollyVideo from 'scrolly-video/dist/ScrollyVideo.vue'
+
+</script>
+
+<template>
+    <main class="h-[2000px]">
+        <div class="fixed h-screen w-screen z-10 p-32">
+            <h1 class="text-white text-9xl drop-shadow-sm">🥳</h1>
+            <p class="text-white/75 text-xs font-bold uppercase tracking-widest absolute bottom-0 mb-32">[Scroll Up and Down]</p>
+        </div>
+        <ScrollyVideo :src="BgVideo" :debug="true" />
+    </main>
+</template>
---
+++ src/types.d.ts
@@ -0,0 +1,1 @@
+declare module 'scrolly-video/dist/ScrollyVideo.vue'
